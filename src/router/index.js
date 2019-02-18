4f14aecd68157accf964373e8127b3150d87f436
李经纶 <281965992@qq.com>
2019-02-18 11:36:47 +0800

    新增了左右结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import main from '@/pages/main.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: '/',
+      component: main,
+      children:  [{
+        path: '/',
+        name: 'test',
+        component: HelloWorld
+      }]
     }
   ]
 })
